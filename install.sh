fd344cbe32778d31398ae46f9d5c7c9f9b21f0d1
Danilo Silva <badkill82@gmail.com>
2015-06-24 22:06:14 +0200

    create symlink for a list of dot files

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files=".bashrc .vimrc .vim"       # list of files/folders to symlink in homedir
+# files=".bashrc .vimrc .vim"       # list of files/folders to symlink in homedir
+files=".tmux.conf"       # list of files/folders to symlink in homedir
 
 ##########
 
@@ -69,22 +70,22 @@ while true; do
     esac
 done
 
-# # create dotfiles_old in homedir
-# echo "Creating $olddir for backup of any existing dotfiles in ~"
-# mkdir -p $olddir
-# echo "...done"
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
 
-# # change to the dotfiles directory
-# echo "Changing to the $dir directory"
-# cd $dir
-# echo "...done"
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
 
-# # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-# for file in $files; do
-#     echo "Moving any existing dotfiles from ~ to $olddir"
-#     mv ~/$file ~/$olddir/
-#     echo "Creating symlink to $file in home directory."
-#     ln -s $dir/$file ~/$file
-# done
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/$file ~/$olddir/
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/$file
+done
 
 # source ~/.bashrc
