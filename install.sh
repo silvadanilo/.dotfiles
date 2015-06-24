e7e5efabcc553ec2076ff8133644456e101b2303
Danilo Silva <badkill82@gmail.com>
2015-06-24 23:36:45 +0200

    added bash aliases to the list of symlink files

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
 # files=".bashrc .vimrc .vim"       # list of files/folders to symlink in homedir
-files=".tmux.conf .fonts .gitconfig .vimrc.local .vimrc.plugins.local"       # list of files/folders to symlink in homedir
+files=".bash_aliases .tmux.conf .fonts .gitconfig .vimrc.local .vimrc.plugins.local"       # list of files/folders to symlink in homedir
 
 ##########
 
