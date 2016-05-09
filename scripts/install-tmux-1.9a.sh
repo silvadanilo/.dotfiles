9227ab30c3c460e5f30a30f92951c56782e067e8
SAMPLE <SAMPLE>
2016-05-09 18:42:21 +0200

    updated tmux-1.9a url (it moved from sourceforge to github)

--- scripts/install-tmux-1.9a.sh
+++ scripts/install-tmux-1.9a.sh
@@ -3,7 +3,8 @@ sudo apt-get install -y libevent-dev libncurses-dev pkg-config
 
 cd /tmp
 rm -rf tmux-1.9a
-wget "http://downloads.sourceforge.net/project/tmux/tmux/tmux-1.9/tmux-1.9a.tar.gz"
+rm -rf tmux-1.9a.tar.gz
+wget "https://github.com/tmux/tmux/releases/download/1.9a/tmux-1.9a.tar.gz"
 tar xvzf tmux-1.9a.tar.gz
 cd tmux-1.9a
 
