02757e3f88d107d61317246df6b1317d05afbf03
Danilo Silva <badkill82@gmail.com>
2018-04-27 22:41:38 +0200

    fixed label

--- scripts/install.sh
+++ scripts/install.sh
@@ -76,7 +76,7 @@ do_install() {
                 echo -e "${LGREEN}...done${Z}\n"
                 ;;
             2)
-                echo -e "${LGREEN}Installing${Z} ${YELLOW}Vim 7.4${Z}"
+                echo -e "${LGREEN}Installing${Z} ${YELLOW}Neovim${Z}"
                 /bin/bash < <(curl -s https://raw.githubusercontent.com/silvadanilo/.dotfiles/master/scripts/install-neovim.sh)
                 echo -e "${LGREEN}...done${Z}\n"
                 ;;
