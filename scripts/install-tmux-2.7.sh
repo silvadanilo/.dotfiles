a7366a075667af8dad321f5d682fcca9525721ca
SAMPLE <SAMPLE>
2018-04-29 15:34:06 +0000

    updated tmux install script to 2.7

--- scripts/install-tmux-2.7.sh
+++ scripts/install-tmux-2.7.sh
@@ -5,13 +5,14 @@ function sudo ()
     "$@"
 }
 
+sudo apt-get remove -y tmux
 sudo apt-get install -y libevent-dev libncurses-dev pkg-config
 
 cd /tmp
-rm -rf tmux-2.2
-wget "https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz"
-tar xvzf tmux-2.2.tar.gz
-cd tmux-2.2
+rm -rf tmux-2.7
+wget "https://github.com/tmux/tmux/releases/download/2.7/tmux-2.7.tar.gz"
+tar xvzf tmux-2.7.tar.gz
+cd tmux-2.7
 
 autoreconf -fis
 ./configure
